300b06627ebe4b1c895ee405bff43304affddbf9
denis-010 <opjl456i@gmail.com>
2019-12-05 18:40:56 +1000

    Version 0.1

--- src/Number.java
+++ src/Number.java
@@ -1,2 +1,21 @@
-public interface Number {
+public abstract class Number {
+    private String question;
+    private String answer;
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+        
 }
